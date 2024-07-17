412c99a4f2952ce90470bb31a474b9924cfdde7d
Athikhom_ra <athikhom_ra@outlook.com>
2024-07-17 14:39:21 +0700

    resolve: ex-1,2 'algorithm-problem#2'

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,42 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  //Binary Search
+  let index = -1;
+  let left = 0,
+    right = students.length - 1;
+  for (let i = 0; i < students.length; i++) {
+    let mid = Math.floor((left + right) / 2);
+    if (students[mid] === searchStudent) {
+      index = mid;
+    } else if (students[mid] > searchStudent) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return index;
 }
-
+console.log(
+  findStudentIndex(
+    [
+      "Andrew",
+      "Bobby",
+      "Diana",
+      "George",
+      "Hannah",
+      "Isaac",
+      "Jasmine",
+      "John",
+    ],
+    "John"
+  )
+);
+console.log(
+  findStudentIndex(
+    ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"],
+    "Andrew"
+  )
+);
+// ยังจำได้ไหมว่า Binary Search มี Big O เป็นเท่าไร เพราะอะไร
 // ตอบคำถามตรงนี้จ้า
+// Binary Search มี Big O เป็น log n เพราะ การวน loop ในการหาคำตอบแต่ละครั้ง จำนวนการค้นหาจะลดลงครึ่งหนึ่งเสมอ
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,31 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  //Binary Search
+  let index = -1;
+  let left = 0,
+    right = books.length - 1;
+  for (let i = 0; i < books.length; i++) {
+    let mid = Math.floor((left + right) / 2);
+    if (books[mid].title === searchTitle) {
+      index = mid;
+    } else if (books[mid].title > searchTitle) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return index;
 }
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+console.log(findBookIndex(books, "Gone with the Wind"));
+console.log(findBookIndex(books, "The Master Mind"));
